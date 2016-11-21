9416579d0138614768293b4ee92b35b857da7e46
Bernard Paques <bernard.paques@gmail.com>
2016-11-21 08:43:33 +0100

    give clear instructions to run this stuff securely

---
+++ tokens.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from hook import configure, load_buttons, generate_tokens
+
+settings = configure('settings.yaml')
+buttons = load_buttons(settings)
+
+if 'key' in settings['server']:
+    tokens = generate_tokens(settings, buttons.keys())
+    for key in tokens:
+        print('{}: {}'.format(key, tokens[key]))
+
+else:
+    print('Add a secret key to settings file if you want security')
+    print('No security token has been generated')
